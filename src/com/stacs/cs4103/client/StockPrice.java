c2eca1ac3dc91ff2010e31d2f41fb162b8b6adf1
Ajay Pradeep Mahadeven <m.ajaypradeep@icloud.com>
2023-03-02 22:23:44 +0000

    initial commit
    Initial commit in github

---
+++ src/com/stacs/cs4103/client/StockPrice.java
@@ -0,0 +1,40 @@
+package com.stacs.cs4103.client;
+
+public class StockPrice {
+    private String symbol;
+    private double price;
+    private double change;
+
+    public StockPrice() {
+    }
+    public  StockPrice(String symbol, double price, double change) {
+        this.symbol = symbol;
+        this.price = price;
+        this.change = change;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public double getChange() {
+        return change;
+    }
+    public double getChangePercent() {
+        return 100.0 * change / price;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setChange(double change) {
+        this.change = change;
+    }
+
+
+}
